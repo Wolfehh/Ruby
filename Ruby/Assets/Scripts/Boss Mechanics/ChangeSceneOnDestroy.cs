7700d676482ea262d560a7e6aad27a6237b1477c
Ethan Fournier <59780951+Wolfehh@users.noreply.github.com>
2022-02-15 20:20:26 -0500

    Nerfed boss and fixed bug at end of the game

--- Ruby/Assets/Scripts/Boss Mechanics/ChangeSceneOnDestroy.cs
+++ Ruby/Assets/Scripts/Boss Mechanics/ChangeSceneOnDestroy.cs
@@ -5,15 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class ChangeSceneOnDestroy : MonoBehaviour
 {
+    Health health;
+    private float CurrHealth;
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = GetComponent<Health>();
     }
 
     private void OnDestroy()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        CurrHealth = health.GetHealth();
+        if (CurrHealth == 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
 
     // Update is called once per frame
